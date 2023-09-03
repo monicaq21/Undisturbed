84f017f641500e4331d26b2a0cfa04dbba99d9be
Monica Qiu <m1102778586@gmail.com>
2023-09-03 17:15:37 -0400

    Set logout feature on profile view

--- Undisturbed/Main/ProfileViewController.swift
+++ Undisturbed/Main/ProfileViewController.swift
@@ -6,12 +6,48 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ProfileViewController: UIViewController {
+    
+    let signoutBtn: UIButton = {
+        let button = UIButton()
+        button.setTitle("Sign Out", for: .normal)
+        button.backgroundColor = .brown
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
+        view.addSubview(signoutBtn)
+        signoutBtn.addTarget(self, action: #selector(signOutBtnTapped), for: .touchUpInside)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        signoutBtn.frame = CGRect(x: 100, y: 100, width: view.width - 200, height: 100)
+    }
+    
+    @objc private func signOutBtnTapped() {
+        do {
+            try Auth.auth().signOut()
+            transitionToLoginScreen()
+        } catch {
+            print("Signout failed")
+        }
+    }
+    
+    private func transitionToLoginScreen() {
+        DispatchQueue.main.async {
+            let vc = UINavigationController(rootViewController: LoginViewController())
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: true) {
+                self.navigationController?.popToRootViewController(animated: false)
+            }
+        }
     }
     
 
